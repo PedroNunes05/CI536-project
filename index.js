d0b8221330227fa0cd8bedc53b59ba8d03a1c9e2
PedroNunes05 <99275823+PedroNunes05@users.noreply.github.com>
2022-05-12 19:00:25 +0100

    Changes that allows connection to database

--- index.js
+++ index.js
@@ -3,37 +3,37 @@ window.addEventListener("load", function(){
 	
 	//********************** the books page *******************
 	document.querySelector("#books").addEventListener("click", function(){ 
-		window.open("books.html", "_self")
+		window.open("book.php", "_self")
 	})
 	
 	
 	//********************** the electronics page *******************
 	document.querySelector("#electronics").addEventListener("click", function(){ 
-		window.open("electronics.html", "_self")
+		window.open("electronics.php", "_self")
 	})
 	
 	
 	//********************** the stationery page *******************
 	document.querySelector("#stationery").addEventListener("click", function(){ 
-		window.open("stationery.html", "_self")
+		window.open("stationery.php", "_self")
 	})
 	
 	
 	//********************** the furniture page *******************
 	document.querySelector("#furniture").addEventListener("click", function(){ 
-		window.open("furniture.html", "_self")
+		window.open("furniture.php", "_self")
 	})
 	
 	
 	//********************** the food page *******************
 	document.querySelector("#food").addEventListener("click", function(){ 
-		window.open("food.html", "_self")
+		window.open("food.php", "_self")
 	})
 	
 	
 	//********************** the gardening page *******************
 	document.querySelector("#gardening").addEventListener("click", function(){ 
-		window.open("gardening.html", "_self")
+		window.open("gardening.php", "_self")
 	})
 	
 	
